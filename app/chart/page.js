fee201bb852737917db4e656d442bc5bf559b59b
Disha <sethidisha123@gmail.com>
2025-03-31 16:18:22 +0530

    changed naming conflicts for chart

--- app/chart/page.js
+++ app/chart/page.js
@@ -1,9 +1,9 @@
-import Chart from "./chart";
+// import Chart from "./chart";
 
-export default function Chart() {
-  return (
-    <div className="flex justify-center items-center min-h-screen bg-gray-100">
-      <Chart />
-    </div>
-  );
-}
+// export default function Chart() {
+//   return (
+//     <div className="flex justify-center items-center min-h-screen bg-gray-100">
+//       <Chart />
+//     </div>
+//   );
+// }
